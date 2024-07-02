e80f964653658ed5cbdde9094032324c2e7a741d
Manikandans1 <manimp377@gmail.com>
2024-07-02 20:03:48 +0530

    Initial commit

---
+++ app/src/main/java/com/lettergame/lettergame/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.lettergame.lettergame
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.lettergame.lettergame.ui.theme.GameScreen
+import com.lettergame.lettergame.ui.theme.GameViewModel
+import com.lettergame.lettergame.ui.theme.LetterGameTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LetterGameTheme {
+                    GameScreen()
+
+            }
+        }
+    }
+}
